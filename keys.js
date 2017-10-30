34ca39be9e9a1fcbcd11573d05d534a9a53dedb4
Manasi <manudee@gmail.com>
2017-10-29 21:03:58 -0400

    twitter api and spotify updated

--- keys.js
+++ keys.js
@@ -1,10 +1,22 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '8qWeBzdryRklLIZtVhc5Ez6R1',
-  consumer_secret: 'B7fGH0MsO1k1nmuuprwwWimM3qLeXInENGEWvz30fwuzhlat7n',
-  access_token_key: '923356805673177093-PhWevWerAa9HLi7YQkz9BLuQVhuqLX4',
-  access_token_secret: 'cJxWAaXZGiVztjNiYKODHsjC2oSw5Il2IIjK86bTaZ2IO',
+	consumer_key: '8qWeBzdryRklLIZtVhc5Ez6R1',
+	consumer_secret: 'B7fGH0MsO1k1nmuuprwwWimM3qLeXInENGEWvz30fwuzhlat7n',
+	access_token_key: '923356805673177093-PhWevWerAa9HLi7YQkz9BLuQVhuqLX4',
+	access_token_secret: 'cJxWAaXZGiVztjNiYKODHsjC2oSw5Il2IIjK86bTaZ2IO',
 }
 
-module.exports = twitterKeys;
+
+var spotifyKeys = {
+
+	clientID: 'b794406c22de43959e97ff27a4599f90',
+	clientSecret: '583000953831420399e3ccc4b82d556b'
+}
+
+
+module.exports = {
+	twitterKeys:twitterKeys,
+	spotifyKeys:spotifyKeys
+}
+
--- liri.js
+++ liri.js
@@ -1,17 +1,106 @@
-var twitterKeys = require('./keys.js')
+var keys = require('./keys.js');
+var fs = require("fs");
+
+console.log("Access token key: " + keys.twitterKeys.access_token_key);
+console.log("Access token secret: " + keys.twitterKeys.access_token_secret);
+console.log("Consumer key: " + keys.twitterKeys.consumer_key);
+console.log("Consumer Secret: " + keys.twitterKeys.consumer_secret);
 
 
-console.log("Access token key: " + twitterKeys.access_token_key);
-console.log("Access token secret: " + twitterKeys.access_token_secret);
-console.log("Consumer key: " + twitterKeys.consumer_key);
-console.log("Consumer Secret: " + twitterKeys.consumer_secret);
+console.log("Client ID for spotify " + keys.spotifyKeys.clientID);
+console.log("Client Secret for spotify " + keys.spotifyKeys.clientSecret);
 
 
 //variables for the keys
-var accessTokenKey = twitterKeys.access_token_key;
-var accessTokenSecret = twitterKeys.access_token_secret;
-var consumerKey = twitterKeys.consumer_key;
-var consumerSecret = twitterKeys.consumer_secret;
+var accessTokenKey = keys.twitterKeys.access_token_key;
+var accessTokenSecret = keys.twitterKeys.access_token_secret;
+var consumerKey = keys.twitterKeys.consumer_key;
+var consumerSecret = keys.twitterKeys.consumer_secret;
 
-var command = process.argv[2];// this will take values from random.txt
+var commandArgs = process.argv;// this will take values from random.txt
 
+
+var request = require('request');
+var spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+
+var dataArr;
+
+var client = new Twitter({
+	consumer_key: consumerKey,
+	consumer_secret: consumerSecret,
+	access_token_key: accessTokenKey,
+	access_token_secret: accessTokenSecret
+});
+
+
+var spotifyModule = new spotify({
+	id: keys.spotifyKeys.clientID,
+	secret: keys.spotifyKeys.clientSecret
+});
+
+// spotifyModule
+//   .search({ type: 'track', query: 'I Want it That Way' })
+//   .then(function(response) {
+//     // console.log(response);
+//     console.log(response.tracks);
+
+
+//     console.log("Artists " + response.tracks.items[0].artists);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+
+//spotify
+spotifyModule.search({ type: 'track', query: 'I Want it That Way' }, function(err, data) {
+	if (err) {
+		return console.log('Error occurred: ' + err);
+	}
+
+	// console.log(data.tracks);
+	
+	for (var i = 0; i < 20 ; i++) {
+		console.log("__________________________________________________Song#" +i+"_____________________________________________________________");
+		console.log("Artist's name: " + data.tracks.items[i].artists[0].name);
+		console.log("Song's Name : " + data.tracks.items[i].name);
+		console.log("Preview url: " + data.tracks.items[i].preview_url);
+		console.log("Album Name " + data.tracks.items[i].album.name);
+	}
+
+});
+
+
+// fs.readFile('random.txt', "utf8",function(error, data){
+// 	if(!error){
+// 		// console.log(data);
+// 		dataArr = data.split(",");
+// 		console.log(dataArr);
+// 	}
+
+
+// })
+
+
+
+
+//Twitter api
+var params = {screen_name: 'mnc8525'};
+client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?count=20', params, function(error, tweets, response) {
+	if (!error) {
+	//console.log(tweets[element].text);	
+ 	//console.log(tweets);
+
+ 	var limit = tweets[0].user.statuses_count;
+ 	//console.log(tweets[0].text);
+ 	//console.log(tweets[0].created_at);
+ 	for (var i = 0; i < limit ; i++) {
+ 		console.log(tweets[i].text +'\n' + " Created at: "+tweets[i].created_at +'\n');
+ 	}
+ }
+});
+
+
+
+//omdb
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
